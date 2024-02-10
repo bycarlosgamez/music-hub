f2cd753b6d5ffafabe476a5695741dfb3660e951
Carlos Gámez <hardpx@gmail.com>
2024-02-09 21:47:21 -0600

    build layout template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      gap={6}
+    >
+      <GridItem area='nav' bg='teal' />
+      <Show above='lg'>
+        <GridItem area='aside' bg='tomato' />
+      </Show>
+      <GridItem area='main' bg='papayawhip' />
+    </Grid>
+  );
 }
 
 export default App;
