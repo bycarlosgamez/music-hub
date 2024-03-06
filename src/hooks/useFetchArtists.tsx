ae63b67b90124604004a3864bd155ca593a76718
Carlos Gámez <hardpx@gmail.com>
2024-03-06 12:53:55 -0600

    fetch artsits custom hook

---
+++ src/hooks/useFetchArtists.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import apiArtistID from '../services/api-artist-id';
+import getRelatedArtsits from '../services/api-artist-related';
+
+const useFetchArtists = (accessToken: string) => {
+  const searchInput = 'yob';
+  const [artistID, setArtsitID] = useState('');
+  const [artists, setArtists] = useState<any[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    apiArtistID(accessToken, searchInput)
+      .then((res) => res.json())
+      .then((data) => {
+        setArtsitID(data.artists.items[0].id);
+      })
+      .catch((err) => setError(err.message));
+  }, [searchInput]);
+
+  useEffect(() => {
+    getRelatedArtsits(accessToken, artistID)
+      .then((res) => res.json())
+      .then((data) => setArtists(data.artists))
+      .catch((err) => setError(err.message));
+  }, [artistID]);
+
+  return { artists, error };
+};
+
+export default useFetchArtists;
